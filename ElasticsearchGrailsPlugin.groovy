dd918513e2ec9c358a4d2e056e4bbbf9463f7843
Eric Martineau <ericm@infusionsoft.com>
2013-07-12 00:26:17 -0700

    Version number

--- ElasticsearchGrailsPlugin.groovy
+++ ElasticsearchGrailsPlugin.groovy
@@ -37,7 +37,7 @@ class ElasticsearchGrailsPlugin {
 
     // the plugin version
 
-    def version = "0.20.6.3.BUILD-SNAPSHOT"
+    def version = "1.0.1.BUILD-SNAPSHOT"
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.0 > *"
